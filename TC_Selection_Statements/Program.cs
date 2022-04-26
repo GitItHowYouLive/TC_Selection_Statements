30eab62536b0bbe1375808897bfe05b9a7c40780
GitItHowYouLive <MicStandTerrence@gmail.com>
2022-04-26 09:40:21 -0500

    Both exercises complete rd

--- TC_Selection_Statements/Program.cs
+++ TC_Selection_Statements/Program.cs
@@ -7,6 +7,7 @@ namespace TC_Selection_Statements
         static void Main(string[] args)
         {
             QuickGame();
+            SwitchStatements();
         }
 
         static void QuickGame()
@@ -31,6 +32,34 @@ namespace TC_Selection_Statements
             else
             {
                 Console.WriteLine("I don't even know how, but you broke it. Great job, I guess.");
+            }
+        }
+
+        static void SwitchStatements()
+        {
+            Console.WriteLine("Welcome to the first day of school! \n Please inform us of your favorite subject so that we can place you in the appropriate 'clique'.");
+            string fav = Console.ReadLine();
+
+            switch (fav)
+            {
+                case "math":
+                    Console.WriteLine("Math? Welcome to the nerds! L337 4 LIFE!");
+                    break;
+                case "english":
+                    Console.WriteLine("So I guess you like words, huh? Guess you can be a theater kid. They have low standards anyway.");
+                    break;
+                case "spanish":
+                    Console.WriteLine("Oh, you exotic or something? You're one of the emo kids now. Congrats.");
+                    break;
+                case "science":
+                    Console.WriteLine("You know how to blow stuff up, so be whoever you want.");
+                    break;
+                case "history":
+                    Console.WriteLine("Really? Who sits around caring about stuff that already happened? You are friends with the bullies.");
+                    break;
+                default:
+                    Console.WriteLine($"I don't even know what {fav} is, so you get to be a jock.");
+                    break;
             }
         }
     }
